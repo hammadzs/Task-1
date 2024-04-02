c9b89103ee1b34dc53bd8abaeff41b9773fff53b
hammadzs <hammad@zetsol.co>
2024-04-02 11:54:44 +0500

    Page Layout Created

--- src/components/SignInFrom.tsx
+++ src/components/SignInFrom.tsx
@@ -1,8 +1,45 @@
+import Link from "next/link"
+
 const SignInFrom = () => {
   return (
-    <div>
-      
-    </div>
+    <form className="space-y-4">
+      <label
+        htmlFor="email"
+        className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+      >
+        <input
+          type="text"
+          id="email"
+          className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 text-base p-4"
+          placeholder="email"
+        />
+
+        <span
+          className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-base text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs"
+        >
+          Email
+        </span>
+      </label>
+      <label
+        htmlFor="password"
+        className="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+      >
+        <input
+          type="text"
+          id="password"
+          className="peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 text-base p-4"
+          placeholder="password"
+        />
+
+        <span
+          className="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-base text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs"
+        >
+          Password
+        </span>
+      </label>
+      <button type="submit" className="bg-[#666CFF] w-full text-white p-3 rounded-lg">LOGIN</button>
+      <h3 className="text-sm text-gray-500 text-center">Don’t have an account? <Link className="text-[#666CFF]" href={'/register'}>Sign Up</Link></h3>
+    </form>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import SignInFrom from "@/components/SignInFrom";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,24 +9,27 @@ export default function Home() {
     <main
       className={`relative flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
-      <div className="shadow-lg ">
-        <h1>
+      <div className="shadow-lg max-w-[450px] w-full p-4 space-y-4">
+        <div className="flex justify-center items-center gap-x-2">
           <Image
-            src={'/img/logo.png'}          
+            src={'/img/logo.png'}
             width={400}
             height={400}
             alt="Logo"
             className="max-w-10 w-full"
           />
-        </h1>
+          <h2 className="text-3xl font-semibold">Taska</h2>
+        </div>
+        <h1 className="text-2xl font-semibold text-[#4C4E64DE] rounded-xl">Welcome to Taska! 👋🏻</h1>
+        <SignInFrom />
       </div>
-     <Image
-     src={'/img/Frame.png'}
-     width={500}
-     height={500}
-     alt="Frame Image"
-     className="max-w-full w-full absolute left-0 bottom-0"
-     />
+      <Image
+        src={'/img/Frame.png'}
+        width={500}
+        height={500}
+        alt="Frame Image"
+        className="max-w-full w-full absolute left-0 bottom-0"
+      />
     </main>
   );
 }
